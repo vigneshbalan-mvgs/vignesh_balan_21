fde2033172d29f8df2584dd108ed25a6da9ab5b7
MVGS <vigneshbalanmvgs2003@gmail.com>
2025-09-19 10:59:18 +0530

    v0.0.18

--- src/Components/ProjectDetails.jsx
+++ src/Components/ProjectDetails.jsx
@@ -55,16 +55,23 @@ const ProjectDetails = ({ project, onClose }) => {
 
             <div className="flex flex-col">
               <div>
-                <h3 className="text-lg sm:text-xl font-bold text-dark-accent dark:text-light-accent mb-3 sm:mb-4 border-b-2 border-dark-accent/50 dark:border-light-accent/50 pb-2">Description</h3>
+                <h3 className="text-lg sm:text-xl font-bold text-dark-accent dark:text-light-accent mb-3 sm:mb-4 border-b-2 border-dark-accent/50 dark:border-light-accent/50 pb-2">
+                  Description
+                </h3>
                 {project.descriptions.map((desc, index) => (
-                  <p key={index} className="text-sm sm:text-base mb-3 sm:mb-4 leading-relaxed">
+                  <p
+                    key={index}
+                    className="text-sm sm:text-base mb-3 sm:mb-4 leading-relaxed"
+                  >
                     {desc}
                   </p>
                 ))}
               </div>
 
               <div className="mt-4 sm:mt-6">
-                <h3 className="text-lg sm:text-xl font-bold text-dark-accent dark:text-light-accent mb-3 sm:mb-4 border-b-2 border-dark-accent/50 dark:border-light-accent/50 pb-2">Technologies</h3>
+                <h3 className="text-lg sm:text-xl font-bold text-dark-accent dark:text-light-accent mb-3 sm:mb-4 border-b-2 border-dark-accent/50 dark:border-light-accent/50 pb-2">
+                  Technologies
+                </h3>
                 <div className="flex flex-wrap gap-2 sm:gap-3">
                   {project.technologies.map((tech, index) => (
                     <span
@@ -87,7 +94,7 @@ const ProjectDetails = ({ project, onClose }) => {
                 key={index}
                 src={image}
                 alt={`${project.projectName} thumbnail ${index + 1}`}
-                className={`w-20 h-14 sm:w-24 sm:h-16 object-cover rounded-lg cursor-pointer transition-all duration-200 ${selectedImage === image ? 'ring-4 ring-dark-accent dark:ring-light-accent scale-105' : 'opacity-60 hover:opacity-100'}`}
+                className={`w-20 h-14 sm:w-24 sm:h-16 object-cover rounded-lg cursor-pointer transition-all duration-200 ${selectedImage === image ? "ring-4 ring-dark-accent dark:ring-light-accent scale-105" : "opacity-60 hover:opacity-100"}`}
                 onClick={() => setSelectedImage(image)}
               />
             ))}
@@ -98,7 +105,7 @@ const ProjectDetails = ({ project, onClose }) => {
                 href={project.links.demo}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-dark-accent hover:bg-opacity-80 text-white font-bold py-2 px-4 sm:py-3 sm:px-6 rounded-lg transition-all duration-300 shadow-lg transform hover:scale-105 text-xs sm:text-sm"
+                className="bg-light-accent dark:bg-ligth-accent hover:bg-opacity-80 text-white font-bold py-2 px-4 sm:py-3 sm:px-6 rounded-lg transition-all duration-300 shadow-lg transform hover:scale-105 text-xs sm:text-sm"
               >
                 Live Demo
               </a>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -18,7 +18,10 @@ const Projects = () => {
   };
 
   return (
-    <div id="projects" className="bg-dark-background text-dark-text dark:bg-light-background dark:text-light-text py-20 px-4 sm:px-6 lg:px-8">
+    <div
+      id="projects"
+      className="bg-dark-background text-dark-text dark:bg-light-background dark:text-light-text py-20 px-4 sm:px-6 lg:px-8"
+    >
       <div className="mx-auto max-w-7xl">
         <motion.h2
           initial={{ opacity: 0, y: -50 }}
@@ -49,18 +52,25 @@ const Projects = () => {
                   className="w-full h-56 object-cover project-card-bg"
                 ></div>
                 <div className="p-4">
-                  <h4 className="text-lg sm:text-xl lg:text-2xl font-bold text-dark-accent dark:text-light-accent mb-2">{project.projectName}</h4>
-                  <p className="text-xs sm:text-sm lg:text-base text-dark-text-secondary dark:text-light-text-secondary mb-4 h-20 overflow-hidden project-description">{project.descriptions[0]}</p>
+                  <h4 className="text-lg sm:text-xl lg:text-2xl font-bold text-dark-accent dark:text-light-accent mb-2">
+                    {project.projectName}
+                  </h4>
+                  <p className="text-xs sm:text-sm lg:text-base text-dark-text-secondary dark:text-light-text-secondary mb-4 h-20 overflow-hidden project-description">
+                    {project.descriptions[0]}
+                  </p>
                 </div>
                 <div className="p-4 bg-dark-background dark:bg-light-background">
                   <div className="flex flex-wrap gap-2">
                     {project.technologies.slice(0, 3).map((tech, techIndex) => (
-                      <span key={techIndex} className="bg-dark-accent text-dark-background dark:bg-light-accent dark:text-light-background text-xxs sm:text-xs lg:text-sm font-semibold px-2 py-1 rounded-full">
+                      <span
+                        key={techIndex}
+                        className="bg-dark-accent text-dark-background dark:bg-light-accent dark:text-light-background text-xxs sm:text-xs lg:text-sm font-semibold px-2 py-1 rounded-full"
+                      >
                         {tech}
                       </span>
                     ))}
                   </div>
-                  <button className="mt-4 w-full bg-dark-accent hover:bg-dark-accent-hover text-white font-bold py-1 px-3 sm:py-2 sm:px-4 rounded">
+                  <button className="mt-4 w-full bg-dark-accent dark:bg-light-accent hover:bg-dark-accent-hover text-white font-bold py-1 px-3 sm:py-2 sm:px-4 rounded">
                     View Details
                   </button>
                 </div>
@@ -89,16 +99,23 @@ const Projects = () => {
                   className="w-full h-56 object-cover project-card-bg"
                 ></div>
                 <div className="p-6">
-                  <h4 className="text-xl sm:text-2xl lg:text-3xl font-bold text-dark-accent dark:text-light-accent mb-2">{project.projectName}</h4>
-                  <p className="text-sm sm:text-base lg:text-lg text-dark-text-secondary dark:text-light-text-secondary mb-4 project-description">{project.descriptions[0]}</p>
+                  <h4 className="text-xl sm:text-2xl lg:text-3xl font-bold text-dark-accent dark:text-light-accent mb-2">
+                    {project.projectName}
+                  </h4>
+                  <p className="text-sm sm:text-base lg:text-lg text-dark-text-secondary dark:text-light-text-secondary mb-4 project-description">
+                    {project.descriptions[0]}
+                  </p>
                   <div className="flex flex-wrap gap-2">
                     {project.technologies.map((tech, techIndex) => (
-                      <span key={techIndex} className="bg-dark-background text-dark-accent dark:bg-light-background dark:text-light-accent text-xs sm:text-sm lg:text-base font-semibold px-2 py-1 rounded">
+                      <span
+                        key={techIndex}
+                        className="bg-dark-background text-dark-accent dark:bg-light-background dark:text-light-accent text-xs sm:text-sm lg:text-base font-semibold px-2 py-1 rounded"
+                      >
                         {tech}
                       </span>
                     ))}
                   </div>
-                   <button className="mt-4 w-full bg-dark-accent hover:bg-dark-accent-hover text-white font-bold py-1 px-3 sm:py-2 sm:px-4 rounded">
+                  <button className="mt-4 w-full bg-dark-accent dark:bg-light-accent hover:bg-dark-accent-hover text-white font-bold py-1 px-3 sm:py-2 sm:px-4 rounded">
                     View Details
                   </button>
                 </div>
@@ -107,7 +124,10 @@ const Projects = () => {
           </div>
         </div>
         <div className="text-center mt-12">
-          <Link to="/projects" className="bg-dark-accent hover:bg-dark-accent-hover text-white font-bold py-2 px-4 sm:py-3 sm:px-6 rounded-lg transition-all duration-300 shadow-lg transform hover:scale-105">
+          <Link
+            to="/projects"
+            className="dark:bg-light-accent hover:bg-dark-accent-hover text-white bg-dark-accent font-bold py-2 px-4 sm:py-3 sm:px-6 rounded-lg transition-all duration-300 shadow-lg transform hover:scale-105"
+          >
             View All Projects
           </Link>
         </div>
